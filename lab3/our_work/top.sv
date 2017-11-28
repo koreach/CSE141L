4baa288e063b9f826508f39bde4ad4461d387e3a
Brianna Torres Yamanaka <briannamarie@brianna.yamanaka>
2017-11-27 17:35:44 -0800

    put stuff into their new directories

---
+++ lab3/our_work/top.sv
@@ -0,0 +1,26 @@
+/*********************************************
+ *  Brianna Yamanaka                         *   
+ *  Jeffrey Trang                            *   
+ *  Goo Gu                                   *   
+ *  Byung Lim                                *   
+ *                                           *   
+ *  CSE141L Lab3                             *   
+ *                                           *   
+ *  TOP module					                     *   
+ *                                           *   
+ *********************************************/
+
+//Refer to LECTURE 7
+module top(
+	input clk,
+		  reset,
+	output logic done
+	);
+
+//You need a program counter
+
+//InstructionROM lookup and how wide is its output. So the lookup table.
+
+//Register file
+
+//ALU
