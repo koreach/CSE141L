542258692d3d2240bdd3095fd32f292234d18874
Jeffrey Trang <Jeffreyhuytrang@yahoo.com>
2017-11-28 15:19:13 -0800

    data_mem commit

--- lab3/our_work/data_mem.sv
+++ lab3/our_work/data_mem.sv
@@ -9,3 +9,31 @@
  *  Data memory module                       *   
  *                                           *   
  *********************************************/
+
+// holds data to be processed or stored into computer storage
+// this is exactly like starter code
+
+module data_mem(
+	input CLK,
+	input [7:0] DataAddress,
+	input ReadMem,
+	input WriteMem,
+	input [7:0] DataIn,
+	output logic [7:0] DataOut
+	);
+
+	logic[7:0] mymemory[2**AW]; //idk what mymemory is yet, TODO
+
+	always_comb
+		if(ReadMem)begin
+			DataOut = mymemory[DataAddress]
+		end
+		else
+			DataOut = 16'bZ;
+			
+	always_ff @ (posedge CLK)	             // writes are clocked / sequential
+    if(WriteMem) begin
+      my_memory[DataAddress] = DataIn;
+	  $display("Memory write M[%d] = %d",DataAddress,DataIn);
+    end
+endmodule
