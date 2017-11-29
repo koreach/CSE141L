f98a978102e458f91672598445fe1bd707578b58
koreach <bg_lim@hotmail.com>
2017-11-29 02:09:52 -0800

    Added couple of lines for lookuptable{

--- lab3/our_work/lookuptable.sv
+++ lab3/our_work/lookuptable.sv
@@ -16,16 +16,102 @@ always_comb
 	case(op_i)
 	//Have this for each of the combinations of the lookup table
 	//No limit to how big you make this thing
+	//PART 1: Int to float
+		//Code: mov r0, #0x06
 		9'h0:	begin
-					mem_w 	= 0'b0;		//mem write (for store)
-					rf_w	= 0'b0;		//register file write (for load)
-					alu_op	= 5'h1;		//telling the alu what to do. You tell ALU to do op 1 here
-				 	rs_addr	= 4'h0;		//point to the two operands into the ALU (both pointers)
-				    rt_addr = 4'h1;		//and the content of the two addresses will go into ALU
-				    rd_addr	= 4'h2;		//Point to where I'd like my ALU to be writing back to. This is written on the next pos edge clock
-					imm_pc	= 16'h000f;	//If you need an immediate, if you are doing conditional branch or jump, move by f units
-					alu_in	= 8'd29;	//The ALU constant
-					alu_sell= 1'b0;		//Whether I am selecting the ALU constant
+					mem_w 	= 0'b0;			//mem write (for store)
+					rf_w	= 0'b0;			//register file write (for load)
+					alu_op	= 5'b01100;		//telling the alu what to do. You tell ALU to do op 1 here
+				 	rs_addr	= 4'h0;			//point to the two operands into the ALU (both pointers)
+				    rt_addr = 4'h0;			//and the content of the two addresses will go into ALU
+				    rd_addr	= 4'h0;			//Point to where I'd like my ALU to be writing back to. This is written on the next pos edge clock
+					imm_pc	= 16'b0;		//If you need an immediate, if you are doing conditional branch or jump, move by f units
+					alu_in	= 8'h06;		//The ALU constant
+					alu_sell= 1'b1;			//Whether I am selecting the ALU constant
+				end
+		//Code: mov r1, #0x80
+		9'h1:	begin
+					mem_w 	= 0'b0;			//mem write (for store)
+					rf_w	= 0'b0;			//register file write (for load)
+					alu_op	= 5'b01100;		//telling the alu what to do. You tell ALU to do op 1 here
+				 	rs_addr	= 4'h0;			//point to the two operands into the ALU (both pointers)
+				    rt_addr = 4'h0;			//and the content of the two addresses will go into ALU
+				    rd_addr	= 4'h1;			//Point to where I'd like my ALU to be writing back to. This is written on the next pos edge clock
+					imm_pc	= 16'b0;		//If you need an immediate, if you are doing conditional branch or jump, move by f units
+					alu_in	= 8'h80;		//The ALU constant
+					alu_sell= 1'b1;			//Whether I am selecting the ALU constant
+				end
+		//Code: and r2, r1, #0x80
+		9'h2:	begin
+					mem_w 	= 0'b0;			//mem write (for store)
+					rf_w	= 0'b0;			//register file write (for load)
+					alu_op	= 5'b01000;		//telling the alu what to do. You tell ALU to do op 1 here
+				 	rs_addr	= 4'h1;			//point to the two operands into the ALU (both pointers)
+				    rt_addr = 4'h0;			//and the content of the two addresses will go into ALU
+				    rd_addr	= 4'h2;			//Point to where I'd like my ALU to be writing back to. This is written on the next pos edge clock
+					imm_pc	= 16'b0;		//If you need an immediate, if you are doing conditional branch or jump, move by f units
+					alu_in	= 8'h80;		//The ALU constant
+					alu_sell= 1'b1;			//Whether I am selecting the ALU constant
+				end
+		//Code: mov r3, #0xFF
+		9'h3:	begin
+					mem_w 	= 0'b0;			//mem write (for store)
+					rf_w	= 0'b0;			//register file write (for load)
+					alu_op	= 5'b01100;		//telling the alu what to do. You tell ALU to do op 1 here
+				 	rs_addr	= 4'h0;			//point to the two operands into the ALU (both pointers)
+				    rt_addr = 4'h0;			//and the content of the two addresses will go into ALU
+				    rd_addr	= 4'h3;			//Point to where I'd like my ALU to be writing back to. This is written on the next pos edge clock
+					imm_pc	= 16'b0;		//If you need an immediate, if you are doing conditional branch or jump, move by f units
+					alu_in	= 8'hFF;		//The ALU constant
+					alu_sell= 1'b1;			//Whether I am selecting the ALU constant
+				end
+		//Code: mov r4, r0
+		9'h4:	begin
+					mem_w 	= 0'b0;			//mem write (for store)
+					rf_w	= 0'b0;			//register file write (for load)
+					alu_op	= 5'b01100;		//telling the alu what to do. You tell ALU to do op 1 here
+				 	rs_addr	= 4'h0;			//point to the two operands into the ALU (both pointers)
+				    rt_addr = 4'h0;			//and the content of the two addresses will go into ALU
+				    rd_addr	= 4'h4;			//Point to where I'd like my ALU to be writing back to. This is written on the next pos edge clock
+					imm_pc	= 16'b0;		//If you need an immediate, if you are doing conditional branch or jump, move by f units
+					alu_in	= 8'h0;		//The ALU constant
+					alu_sell= 1'b0;			//Whether I am selecting the ALU constant
+				end
+		//Code: and r2, r1, #0x80
+		9'h5:	begin
+					mem_w 	= 0'b0;			//mem write (for store)
+					rf_w	= 0'b0;			//register file write (for load)
+					alu_op	= 5'b01000;		//telling the alu what to do. You tell ALU to do op 1 here
+				 	rs_addr	= 4'h1;			//point to the two operands into the ALU (both pointers)
+				    rt_addr = 4'h0;			//and the content of the two addresses will go into ALU
+				    rd_addr	= 4'h2;			//Point to where I'd like my ALU to be writing back to. This is written on the next pos edge clock
+					imm_pc	= 16'b0;		//If you need an immediate, if you are doing conditional branch or jump, move by f units
+					alu_in	= 8'h80;		//The ALU constant
+					alu_sell= 1'b1;			//Whether I am selecting the ALU constant
+				end
+		//Code: forloop 		this is the jump back address but I do not know how will we do this yet... with branching
+		9'h6:	begin
+					mem_w 	= 0'b0;			//mem write (for store)
+					rf_w	= 0'b0;			//register file write (for load)
+					alu_op	= 5'b01000;		//telling the alu what to do. You tell ALU to do op 1 here
+				 	rs_addr	= 4'h1;			//point to the two operands into the ALU (both pointers)
+				    rt_addr = 4'h0;			//and the content of the two addresses will go into ALU
+				    rd_addr	= 4'h2;			//Point to where I'd like my ALU to be writing back to. This is written on the next pos edge clock
+					imm_pc	= 16'b0;		//If you need an immediate, if you are doing conditional branch or jump, move by f units
+					alu_in	= 8'h80;		//The ALU constant
+					alu_sell= 1'b1;			//Whether I am selecting the ALU constant
+				end
+		//Code: forloop 		this is the jump back address but I do not know how will we do this yet... with branching
+		9'h7:	begin
+					mem_w 	= 0'b0;			//mem write (for store)
+					rf_w	= 0'b0;			//register file write (for load)
+					alu_op	= 5'b01000;		//telling the alu what to do. You tell ALU to do op 1 here
+				 	rs_addr	= 4'h1;			//point to the two operands into the ALU (both pointers)
+				    rt_addr = 4'h0;			//and the content of the two addresses will go into ALU
+				    rd_addr	= 4'h2;			//Point to where I'd like my ALU to be writing back to. This is written on the next pos edge clock
+					imm_pc	= 16'b0;		//If you need an immediate, if you are doing conditional branch or jump, move by f units
+					alu_in	= 8'h80;		//The ALU constant
+					alu_sell= 1'b1;			//Whether I am selecting the ALU constant
 				end
 	endcase
 endmodule
