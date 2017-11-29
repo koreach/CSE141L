6675a97d559c1caf2015230d2f2c790ee25d62aa
Jeffrey Trang <jhtrang@ucsd.edu>
2017-11-28 18:11:33 -0800

    updated alu

--- lab3/our_work/ALU.sv
+++ lab3/our_work/ALU.sv
@@ -15,11 +15,11 @@
 /* import definition */
 
 module ALU(
-	input [8:0] instruction9bit; //what part of the instruction/code is the code? 
-	input [7:0] reg_input;
+	input [8:0] instruction9bit, //what part of the instruction/code is the code? 
+	input [7:0] reg_input,
 
-	output logic [7:0] alu_output;
-	output logic overflow_o; //what is this ? this was in starter code?
+	output logic [7:0] alu_output,
+	output logic overflow_o //what is this ? this was in starter code?
 	);
 
 always_comb
@@ -28,14 +28,10 @@ always_comb
 		overflow_o = 'd0; // or alu_output = 8'b0000_0000 or alu_output = 0
 
 		case(instruction9bit)
-			'd0:
-				alu_output = 0 //do something with look up table
-			'd1:
-				alu_output = 0 //do something with look up table
-			'd2:
-				alu_output = 0 //do something with look up table
-			'd3:
-				alu_output = 0 //do something with look up table
+			'd0: alu_output = 0; //do something with look up table
+			'd1: alu_output = 0; //do something with look up table
+			'd2: alu_output = 0; //do something with look up table
+			'd3: alu_output = 0; //do something with look up table
 			//We need more case statements for every outcome
 
 		endcase
