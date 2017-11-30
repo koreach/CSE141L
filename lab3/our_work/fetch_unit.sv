198fa6fb1d19518539311bb68b55365353cc5aab
Jeffrey Trang <jhtrang@ucsd.edu>
2017-11-29 17:28:21 -0800

    updated fetch

--- lab3/our_work/fetch_unit.sv
+++ lab3/our_work/fetch_unit.sv
@@ -10,3 +10,25 @@
  *  and instruction memory			         *
  *                                           *   
  *********************************************/
+module fetch_unit(
+	input clk,
+	input init,
+	input stop,
+	input branch,
+	input [2:0] PC_target_value,
+	input[8:0] instruction
+);
+
+IF fetch(
+	.clk(clk),
+	.init(init),
+	.stop(stop),
+	.branch(branch)
+);
+
+InstRom rom(
+	.target(PC_target_value),
+	.inst(instruction)
+);
+
+endmodule
